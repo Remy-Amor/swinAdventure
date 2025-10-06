01dd8c75d5f4f364909771be678391f07146653a
Remy-Amor <105914423@student.swin.edu.au>
2025-10-06 21:12:38 +1100

    starting on bag unit testing

--- testSwinAdventure/BagTests.cs
+++ testSwinAdventure/BagTests.cs
@@ -13,7 +13,49 @@ namespace TestSwinAdventure
           [SetUp]
           public void Setup()
           {
+               _testToolBag = new Bag(["Tool Bag", "Bag"], "Tool Bag", "A tool bag");
+               _testFoodBag = new Bag(["Food bag", "another bag"], "Food bag", "Description for food bag");
+               _testItem1 = new Item(["Hammer", "tool"], "hammer", "This is a hammer");
+               _testItem2 = new Item(["Apple", "Food"], "apple", "This is an apple");
+          }
+
+          [Test]
+          public void BagLocatesItems()
+          {
+               _testToolBag.Inventory.Put(_testItem1);
+               Assert.That(_testToolBag.Locate("Hammer"), Is.EqualTo(_testItem1));
+          }
+
+          [Test]
+          public void BagLocatesItself()
+          {
 
           }
+
+          [Test]
+          public void BagLocatesNothing()
+          {
+
+          }
+
+          [Test]
+          public void BagFullDescription()
+          {
+
+          }
+
+          [Test]
+          public void BaginBag()
+          {
+
+          }
+
+          [Test]
+          public void BagPrivilegedItem()
+          {
+
+          }
+
+
      }
 }
--- testSwinAdventure/obj/Debug/net9.0/testSwinAdventure.AssemblyInfo.cs
+++ testSwinAdventure/obj/Debug/net9.0/testSwinAdventure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testSwinAdventure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5705e9ab0735f6c6d7d75957cb78367aa5193c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df01ff3381c6fe4797d089d645f2c560ffd03683")]
 [assembly: System.Reflection.AssemblyProductAttribute("testSwinAdventure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testSwinAdventure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
