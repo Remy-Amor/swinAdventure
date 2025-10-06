fdf3a1adb3d0b1a6ade96a9ce9beca80ecf02412
Remy-Amor <105914423@student.swin.edu.au>
2025-10-06 20:54:45 +1100

    created locate and fulldescription methods

--- Bag.cs
+++ Bag.cs
@@ -5,14 +5,37 @@ namespace SwinAdventure
           private Inventory _inventory = new Inventory();
 
           // constructor
-          public Bag(string[] ids, string name, string desc) : base(ids, name, desc) { }
-          
+          public Bag(string[] ids, string name, string desc) : base(ids, name, desc)
+          { }
+
+
           public Inventory Inventory
           {
                get
                {
                     return _inventory;
                }
+          }
+
+          public GameObject Locate(string id)
+          {
+               if (AreYou(id))
+               {
+                    return this;
+               }
+               else if (Inventory.HasItem(id))
+               {
+                    return Inventory.Fetch(id);
+               }
+               else
+               {
+                    return null;
+               }
+          }
+
+          public override string FullDescription
+          {
+               get { return "In the " + Name + " you can see:\n" + Inventory.ItemList; }
           }
      }
 }
